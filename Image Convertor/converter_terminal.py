eb91c81d1c5cf2228bdd70a22c9ae06789cf94b5
RevanthKumar4 <149768277+RevanthKumar4@users.noreply.github.com>
2024-11-21 01:12:08 +0530

    Update converter_terminal.py

--- Image Convertor/converter_terminal.py
+++ Image Convertor/converter_terminal.py
@@ -1,3 +1,3 @@
 from PIL import Image
-im1 = Image.open('input.jpeg')  # takes input image from present folder
-im1.save('output.png')          # output image is generated the folder
+im1 = Image.open('input.jpeg')  
+im1.save('output.png')         
